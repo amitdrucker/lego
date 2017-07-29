8abd004c3098a0891bbcae0a306b4c390d4993da
amitdrucker <amitdrucker@gmail.com>
2017-07-30 00:30:54 +0300

    updated scripts

--- server/app/routes.js
+++ server/app/routes.js
@@ -38,14 +38,31 @@ module.exports = function (app) {
     });
 
     function createNewClientData(id) {
-        var brick = allBricks[Math.round(Math.random() * allBricksCount)];
+        var brick = allBricks[Math.round(Math.random() * allBricksCount)],
+            excluded = currentProcesses[id] ? currentProcesses[id].excluded : {};
         currentProcesses[id] =
             {
                 models: [],
-                excluded: {},
+                excluded: excluded,
                 matches: []
             };
         currentProcesses[id].models = JSON.parse(JSON.stringify(modelsInBrick[brick]));
+        return currentProcesses[id];
+    }
+
+    function findBrick(clientData, res) {
+        while (clientData.models.length > 0) {
+            bricksInModel[clientData.models[0]].forEach(function (brick) {
+                if (!clientData.excluded[brick]) {
+                    res.brick = brick;
+                    return true;
+                }
+            });
+            if (res.brick) {
+                return res.brick;
+            }
+            clientData.models.splice(0, 1);
+        }
     }
 
     function handleResponse(id, contains, brick) {
@@ -59,46 +76,43 @@ module.exports = function (app) {
                     clientData.models.splice(i, 1);
                 }
             }
-            if (!clientData.models) {
+            if (clientData.models.length === 0) {
                 res.msg = 'none found, starting again';
-                clientData.models.push(modelNames[Math.round(Math.random() * numOfModels)]);
-                clientData.excluded = {};
-                res.brick = bricksInModel[clientData.models[0]][0];
-            }
-        } else {
-            // if !brick this is a new search
-            if (brick) {
-                clientData.excluded[brick] = brick;
-                var minRemaining, minRemainingName;
-                for (i = clientData.models.length - 1; i >= 0; i--) {
-                    var model = clientData.models[i];
-                    var remaining = bricksInModel[model].length - Object.keys(clientData.excluded).length;
-                    if (typeof minRemaining === 'undefined' || remaining < minRemaining) {
-                        minRemaining = remaining;
-                        minRemainingName = model;
-                    }
-                    if (remaining === 0) {
-                        res.matches += 1;
-                        clientData.matches.push(model);
-                        clientData.models.splice(i, 1);
-                    }
+                while (!res.brick) {
+                    clientData = createNewClientData(id, res);
+                    findBrick(clientData, res);
                 }
-                res.minRemaining = minRemaining;
-                res.minRemainingName = minRemainingName;
             }
-            if (clientData.models) {
-                bricksInModel[clientData.models[0]].some(function (brick) {
-                    if (!clientData.excluded[brick]) {
-                        res.brick = brick;
-                        return true;
-                    }
-                });
+        }
+
+        // if !brick this is a new search
+        if (brick && contains) {
+            clientData.excluded[brick] = brick;
+        }
+        var minRemaining, minRemainingName;
+        for (i = clientData.models.length - 1; i >= 0; i--) {
+            var model = clientData.models[i];
+            var remaining = bricksInModel[model].length - Object.keys(clientData.excluded).length;
+            if (typeof minRemaining === 'undefined' || remaining < minRemaining) {
+                minRemaining = remaining;
+                minRemainingName = model;
             }
+            if (remaining === 0) {
+                res.matches += 1;
+                clientData.matches.push(model);
+                clientData.models.splice(i, 1);
+            }
+        }
+        res.minRemaining = minRemaining;
+        res.minRemainingName = minRemainingName;
+
+        if (clientData.models.length > 0) {
+            findBrick(clientData, res);
         }
         return res;
     }
 
-    // application -------------------------------------------------------------
+// application -------------------------------------------------------------
     app.get('*', function (req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
